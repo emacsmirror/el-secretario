66dbcf892d1129d08af65445cb91a3c48df2fccb
Leo Okawa Eriscon <leo.ericson@yahoo.se>
2020-12-26 17:45:45 +0100

    Minor refactoring of task chooser

--- el-secretario-tasks.el
+++ el-secretario-tasks.el
@@ -23,29 +23,39 @@
   (--> (org-element-headline-parser (line-end-position))
        (nth 1 it)
        (plist-put it :file-name (buffer-file-name))
-       (plist-put it :buffer (current-buffer))))
+       (plist-put it :buffer (current-buffer))
+       (plist-put it :ELSECRETARIOPRIORITY
+                  (string-to-number (plist-get
+                                     it
+                                     :ELSECRETARIOPRIORITY)))))
 
 (defun el-secretario-tasks-choose-task ()
   (interactive)
   (let* ((files (list "/tmp/test.org"))
-         (task-list (progn
-                      (dolist (f files)
-                        (with-current-buffer (find-file-noselect f)
-                          (set-buffer-modified-p t)
-                          (basic-save-buffer)))
-                      (org-ql-select files '(property "ELSECRETARIOPRIORITY")
-                        :action #'el-secretario-tasks--query-action
-                        :sort (lambda (x y)
-                                (< (string-to-number (plist-get x :ELSECRETARIOPRIORITY))
-                                   (string-to-number (plist-get y :ELSECRETARIOPRIORITY))))))))
+         (task-list
+          ;; Bypass the caching for org-ql as it breaks some stuff
+          (progn
+            (dolist (f files)
+              (with-current-buffer (find-file-noselect f)
+                (set-buffer-modified-p t)
+                (basic-save-buffer)))
+            (org-ql-select files '(property "ELSECRETARIOPRIORITY")
+              :action #'el-secretario-tasks--query-action
+              :sort (lambda (x y)
+                      (< (plist-get x :ELSECRETARIOPRIORITY)
+                         (plist-get y :ELSECRETARIOPRIORITY)))))))
     (dolist (task task-list)
       (switch-to-buffer  (plist-get task :buffer))
       (save-restriction
         (widen)
         (goto-char (plist-get task :begin))
         (org-narrow-to-subtree)
-        (when (y-or-n-p "Do you want to work on this task? ")
-          (break))))))
+        (if (y-or-n-p "Do you want to work on this task? ")
+            (break)
+          (let ((priority (plist-get task :ELSECRETARIOPRIORITY)))
+            (org-set-property "ELSECRETARIOPRIORITY"
+                              (number-to-string (+ (max (round  (* 0.2 priority)) 1)
+                                                   priority)))))))))
 
 (dolist (x '(1 2 3))
   (when (= x 2)
