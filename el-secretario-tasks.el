20a15eb13629d6e275ee5deb3a0a5a7bb975eaa9
Leo Okawa Eriscon <leo.ericson@yahoo.se>
2020-12-28 16:14:15 +0100

    [WIP] Rewrite to use el-secretario's framework

--- el-secretario-tasks.el
+++ el-secretario-tasks.el
@@ -19,6 +19,22 @@
 ;;
 ;;; Code:
 
+
+(defmacro el-secretario-tasks-make-source (query files &optional hydra)
+  "QUERY is an arbitrary org-ql query. FILES is the files to search through.
+NEXT-ITEM-HOOk is called on each heading.
+HYDRA is an hydra to use during review of this source."
+  `(make-el-secretario-source
+    :init-function  (lambda () (el-secretario-tasks--init (quote ,query) (quote ,files) ))
+    :next-function
+    :prev-function
+    :hydra-body
+    :finished-hook #'widen
+    :next-item-hook (lambda ())) )
+
+(defhydra el-secretario-tasks-hydra ()
+  ("s"))
+
 (defun el-secretario-tasks--parse-headline ()
   "Parse headline at point and put in some more relevant information"
   (--> (org-element-headline-parser (line-end-position))
@@ -29,6 +45,53 @@
                   (string-to-number (plist-get
                                      it
                                      :EL-SECRETARIO-PRIORITY)))))
+
+(defvar el-secretario-tasks--tasks-left nil)
+(defvar el-secretario-tasks--tasks-skipped nil)
+
+(defun el-secretario-tasks--init (query files)
+  "TODO"
+  (interactive)
+  ;; Bypass the caching for org-ql as it breaks some stuff
+  (dolist (f files)
+    (with-current-buffer (find-file-noselect f)
+      (set-buffer-modified-p t)
+      (basic-save-buffer)))
+  ;; Sort according to priority
+  (setq el-secretario-tasks--tasks-left
+        (org-ql-select files `(and (property "EL-SECRETARIO-PRIORITY")
+                                   ,query)
+          :action #'el-secretario-tasks--parse-headline
+          :sort (lambda (x y)
+                  (< (plist-get x :EL-SECRETARIO-PRIORITY)
+                     (plist-get y :EL-SECRETARIO-PRIORITY)))))
+  (setq el-secretario-tasks--items-done nil)
+  ;; TODO call Next item
+  (funcall (funcall (el-secretario-source-hydra-body
+            (car el-secretario-current-source-list)))))
+
+(defun el-secretario-tasks--skip-task (&optional decrease-priority)
+  (if-let ((task (pop el-secretario-tasks--items-left)))
+      (progn
+        (push task el-secretario-tasks--tasks-skipped)
+        (switch-to-buffer (plist-get task :buffer))
+        (save-restriction
+          (widen)
+          (goto-char (plist-get task :begin))
+          (org-narrow-to-subtree)
+
+          (when decrease-priority
+            (let ((priority (plist-get task :EL-SECRETARIO-PRIORITY)))
+              (org-set-property "EL-SECRETARIO-PRIORITY"
+                                (number-to-string (+ (max (round  (* 0.2 priority)) 1)
+                                                     priority)))))
+          (funcall (el-secretario-source-hydra-body
+                    (car el-secretario-current-source-list)))))
+    (el-secretario--next-source)))
+
+
+(defun el-secretario-tasks--begin-task ()
+  (el-secretario-tasks--run-begin-task-hook))
 
 (defun el-secretario-tasks-choose-task ()
   "Choose a task based on a priority queue.
@@ -65,6 +128,7 @@ dismissed will get their priority decreased.
                                                      priority))))
           (el-secretario-tasks--run-begin-task-hook task)
           (return))))))
+
 (defun el-secretario-tasks--normalize-priorities (tasks)
   (-->
    (-min-by (-on #'> (lambda (x) (plist-get x :EL-SECRETARIO-PRIORITY))) tasks)
