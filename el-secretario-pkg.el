26ab810f90ebb09a54ed10f24014b291f5bbfd06
Leo Okawa Ericson <git@relevant-information.com>
2021-05-31 20:10:45 +0200

    Update to require Emacs 27.1

--- el-secretario-pkg.el
+++ el-secretario-pkg.el
@@ -1,6 +1,6 @@
 (define-package "el-secretario" "0.0.1"
   "The Emacs secretary"
-  '((emacs 26.1)
+  '((emacs 27.1)
     (cl-lib "0.5")
     (hydra "0.15.0")
     (org-ql "0.6-pre")))
--- tests/test-el-secretario.el
+++ tests/test-el-secretario.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords:
 ;; Homepage: https://git.sr.ht/~zetagon/el-secretario
-;; Package-Requires: ((emacs 26.1) (cl-lib "0.5"))
+;; Package-Requires: ((emacs 27.1) (cl-lib "0.5"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
