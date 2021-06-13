215ecaa5ca6ec54878a131b03299807eb142123c
Leo Okawa Ericson <git@relevant-information.com>
2021-06-13 20:36:49 +0200

    Temporarily hide hercules when the minibuffer is showing

--- el-secretario.el
+++ el-secretario.el
@@ -192,5 +192,101 @@ shuffling is done in place."
                 (el-secretario--swap LIST i j)))
   LIST)
 
+;;; Patch for hercules' intereraction with minibuffer
+;;; Remove when https://github.com/wurosh/hercules/pull/2 is merged
+
+(define-advice hercules--hide (:before (&rest _))
+  (apply #'hercules--remove-hooks hercules--temporary-hide-hooks))
+(define-advice hercules--show (:before (&optional keymap flatten transient &rest _))
+  (setq hercules--show-arguments (list keymap flatten transient))
+  (apply #'hercules--add-hooks hercules--temporary-hide-hooks))
+
+(defvar hercules--temporary-restore-hooks
+  '(hercules--restore-after-minibuffer
+    ((:hook minibuffer-exit-hook)))
+  "Hooks for showing hercules after temporarily hiding.
+
+CAR should be `hercules--restore-after-minibuffer'. CDR is a list
+of hooks (as defined by `hercules--add-hooks') for when to show
+hercules after temporarily hiding it with
+`hercules--temporary-hide-hooks'.
+
+Call by
+`(apply hercules--add-hooks hercules--temporary-hide-hooks)'
+or
+`(apply hercules--remove-hooks hercules--temporary-hide-hooks)'
+")
+
+(defvar hercules--temporary-hide-hooks
+  '(hercules--hide-before-minibuffer
+    ((:function read-string)
+     (:function read-from-minibuffer)))
+  "Hooks for temporarily hiding hercules.
+
+CAR should be hercules--hide-before-minibuffer. CDR is a list of
+hooks (as defined by `hercules--add-hooks') for when to
+temporarily hide hercules. Also see
+`hercules--temporary-restore-hooks'.
+
+Call by
+`(apply hercules--add-hooks hercules--temporary-hide-hooks)'
+or
+`(apply hercules--remove-hooks hercules--temporary-hide-hooks)'
+")
+(defun hercules--remove-hooks (fun hooks)
+  "Add FUN to HOOKS.
+
+HOOKS is a (TYPE SYM) plist. If KEY is :hook remove FUN from the hook
+SYM. If KEY is :function remove FUN as :before advice from SYM.
+"
+  (dolist (x hooks)
+    (if-let ((hook (plist-get x :hook)))
+        (remove-hook hook fun))
+    (if-let ((sym (plist-get x :function)))
+        (advice-remove sym fun))))
+(defun hercules--remove-hooks (fun hooks)
+  "Add FUN to HOOKS.
+
+HOOKS is a (TYPE SYM) plist. If KEY is :hook remove FUN from the hook
+SYM. If KEY is :function remove FUN as :before advice from SYM.
+"
+  (dolist (x hooks)
+    (if-let ((hook (plist-get x :hook)))
+        (remove-hook hook fun))
+    (if-let ((sym (plist-get x :function)))
+        (advice-remove sym fun))))
+
+(defun hercules--add-hooks (fun hooks)
+  "Add FUN to HOOKS.
+
+HOOKS is a (TYPE SYM) plist. If KEY is :hook add FUN to the hook
+SYM. If KEY is :function add FUN as :before advice to SYM.
+"
+  (dolist (x hooks)
+    (if-let ((hook (plist-get x :hook)))
+        (add-hook hook fun))
+    (if-let ((sym (plist-get x :function)))
+        (advice-add sym :before fun))))
+
+(defun hercules--hide-before-minibuffer (&rest _)
+  "Temporarily hide hercules.el when the minibuffer is shown.
+
+See `hercules--temporary-hide-hooks'"
+  (apply #'hercules--remove-hooks hercules--temporary-hide-hooks)
+  (apply #'hercules--add-hooks hercules--temporary-restore-hooks)
+  (apply #'hercules--hide hercules--show-arguments))
+
+(defvar hercules--show-arguments nil
+  "The arguments `hercules--show' was last called with.")
+(defun hercules--restore-after-minibuffer ()
+  "Show hercules.el after temporarily hiding when the minibuffer is shown.
+
+See `hercules--temporary-restore-hooks'"
+  ;; This timer is needed. Otherwise hercules will for some reason hide
+  ;; immedeately after being shown.
+  (run-with-timer 0.001 nil
+                  (lambda ()
+                    (apply #'hercules--remove-hooks hercules--temporary-restore-hooks)
+                    (apply #'hercules--show hercules--show-arguments))))
 (provide 'el-secretario)
 ;;; el-secretario.el ends here
