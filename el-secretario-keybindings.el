7575285709e33bf8230ab3a9e49ff5836884c034
Leo Okawa Ericson <git@relevant-information.com>
2021-07-30 12:11:14 +0200

    Eval define-keys after loading their respective module

--- el-secretario-keybindings.el
+++ el-secretario-keybindings.el
@@ -36,58 +36,64 @@
 ;;; Code:
 (require 'general)
 
-(general-define-key
- :keymaps 'el-secretario-source-default-map
- "n" '(el-secretario-next-item :which-key "next")
- "p" '(el-secretario-previous-item :which-key "previous"))
+(eval-after-load 'el-secretario-source
+  (general-define-key
+   :keymaps 'el-secretario-source-default-map
+   "n" '(el-secretario-next-item :which-key "next")
+   "p" '(el-secretario-previous-item :which-key "previous")))
 
-(general-define-key
- :keymaps 'el-secretario-mu4e-map
- "n" '(el-secretario-next-item :which-key "next")
- "p" '(el-secretario-previous-item :which-key "previous"))
+(eval-after-load 'el-secretario-mu4e
+  (general-define-key
+   :keymaps 'el-secretario-mu4e-map
+   "n" '(el-secretario-next-item :which-key "next")
+   "p" '(el-secretario-previous-item :which-key "previous")))
 
-(general-define-key
- :keymaps 'el-secretario-elfeed-map
- "n" '(el-secretario-next-item :which-key "next")
- "p" '(el-secretario-previous-item :which-key "previous")
- "+" '(elfeed-show-tag :which-key "Add tag")
- "-" '(elfeed-show-untag :which-key "Remove tag")
- "c" '(org-capture :which-key "Org Capture")
- "o" '(elfeed-show-visit :which-key "Open in browser"))
+(eval-after-load 'el-secretario-elfeed
+  (general-define-key
+   :keymaps 'el-secretario-elfeed-map
+   "n" '(el-secretario-next-item :which-key "next")
+   "p" '(el-secretario-previous-item :which-key "previous")
+   "+" '(elfeed-show-tag :which-key "Add tag")
+   "-" '(elfeed-show-untag :which-key "Remove tag")
+   "c" '(org-capture :which-key "Org Capture")
+   "o" '(elfeed-show-visit :which-key "Open in browser")))
 
-(general-define-key
- :keymaps 'el-secretario-tasks-keymap
- "s" '((lambda () (interactive) (el-secretario-tasks--skip-task t)) :which-key "Skip task")
- "b" '(el-secretario-tasks-begin-task :which-key "Begin task")
- "t" '((lambda () (interactive) (el-secretario-message--with-pre-buffer (org-todo))) :which-key "TODO" ))
+(eval-after-load 'el-secretario-tasks
+  (general-define-key
+   :keymaps 'el-secretario-tasks-keymap
+   "s" '((lambda () (interactive) (el-secretario-tasks--skip-task t)) :which-key "Skip task")
+   "b" '(el-secretario-tasks-begin-task :which-key "Begin task")
+   "t" '((lambda () (interactive) (el-secretario-message--with-pre-buffer (org-todo))) :which-key "TODO" )))
 
-(general-define-key
- :keymaps 'el-secretario-org-keymap
- "n" '(el-secretario-next-item :which-key "next")
- "p" '(el-secretario-previous-item :which-key "previous")
- "r" '((lambda () (interactive) (org-refile) (el-secretario-next-item)) :wk "Refile")
- "R" '((lambda () (interactive)
-         (let ((org-reverse-note-order t))
-           (org-refile)
-           (el-secretario-next-item))) :wk "Refile to top")
- "t" '(org-set-tags-command :wk "Tags")
- "T" '(org-todo :wk "Tags")
- "s" '(el-secretario-org-schedule :wk "Schedule")
- "z" '(el-secretario-org-ignore-current-item :wk "Snooze")
- "d" '(el-secretario-org-deadline :wk  "Deadline")
- "D" '((lambda () (interactive)
-         (save-restriction
-           (org-narrow-to-subtree)
-           (delete-region (point-min) (point-max))))
-       :wk "Delete visible")
- "q" '((lambda () (interactive) (el-secretario-end-sesion)) :wk "Quit"))
+(eval-after-load 'el-secretario-org
+  (general-define-key
+   :keymaps 'el-secretario-org-keymap
+   "n" '(el-secretario-next-item :which-key "next")
+   "p" '(el-secretario-previous-item :which-key "previous")
+   "r" '((lambda () (interactive) (org-refile) (el-secretario-next-item)) :wk "Refile")
+   "R" '((lambda () (interactive)
+           (let ((org-reverse-note-order t))
+             (org-refile)
+             (el-secretario-next-item))) :wk "Refile to top")
+   "t" '(org-set-tags-command :wk "Tags")
+   "T" '(org-todo :wk "Tags")
+   "s" '(el-secretario-org-schedule :wk "Schedule")
+   "z" '(el-secretario-org-ignore-current-item :wk "Snooze")
+   "d" '(el-secretario-org-deadline :wk  "Deadline")
+   "D" '((lambda () (interactive)
+           (save-restriction
+             (org-narrow-to-subtree)
+             (delete-region (point-min) (point-max))))
+         :wk "Delete visible")
+   "q" '((lambda () (interactive) (el-secretario-end-sesion)) :wk "Quit"))
 
-(hercules-def
- :keymap 'el-secretario-org-keymap)
+  (hercules-def
+   :keymap 'el-secretario-org-keymap))
 
-(general-define-key
- :keymaps 'el-secretario-message-message-keymap
- "q" '(el-secretario-message--back-to-pre-message :which-key "quit" ) )
+(eval-after-load 'el-secretario-message
+  (general-define-key
+   :keymaps 'el-secretario-message-message-keymap
+   "q" '(el-secretario-message--back-to-pre-message :which-key "quit" ) ))
 
 (defun el-secretario-org-schedule (arg &optional time)
   (interactive "P")
